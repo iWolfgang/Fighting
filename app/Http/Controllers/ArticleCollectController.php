3ae2e556127a3d06432ecb39b8c5633b1f389c00
liuran <95473746@qq.com>
2018-04-20 13:49:10 +0800

    conllect_reply

--- app/Http/Controllers/ArticleCollectController.php
+++ app/Http/Controllers/ArticleCollectController.php
@@ -41,4 +41,38 @@ class ArticleCollectController extends Controller
         }
 
 	}
+
+    /**
+     * 文章收藏列表 
+     * Author Amber
+     * Date 2018-04-19
+     * Params [params]
+     * @param  Request $Request [description]
+     * @return [type]           [description]
+     */
+    public function Art_col_reply(Request $Request)
+    {
+        $user_id = $Request->input("user_id");
+
+        if(empty($user_id)){
+            $res = array(
+                "errNo" => "0002",
+                "errMsg" => "缺少必要的参数"
+            );
+            $this->_response($res);
+        }
+
+        $ArticleModel = new ArticleCollectModel();
+
+        $ret = $ArticleModel->Show_collect_reply($user_id);
+        if($ret == FALSE){
+            $res = array(
+                "errNo" => "0003",
+                "errMsg" => "系统错误"
+            );
+            $this->_response($res);
+        }elseif(isset($ret['errNo'])){
+            $this->_response($ret);
+        }
+    }
 }
