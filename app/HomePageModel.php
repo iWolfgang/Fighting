e37f4894c70cdc2469fb40352c8cf2b78a73a4d4
liuran <95473746@qq.com>
2018-05-08 18:09:36 +0800

    HomePage slideshow

---
+++ app/HomePageModel.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use DB;
+
+class HomePageModel extends Model
+{
+    public $_tabName = 't_slideshow';
+
+    /**
+     * 展示轮播图 
+     * Author Amber
+     * Date 2018-05-08
+     * Params [params]
+     
+     */
+    public function slideshow($slideshow_type = '')
+    {
+        if($slideshow_type != 1){
+            
+            return False;
+        }
+        $data = DB::table($this->_tabName)
+        ->where('type', 1)
+        ->get(['id','slideshow','slideshow_url']);
+
+        return $data;
+        
+    }
+
+  /**
+   * 添加轮播图 
+   * Author Amber
+   * Date 2018-05-08
+   * @param  string $slideshow      [图片信息]
+   * @param  string $slideshow_urll [将要天转的路径]
+   */
+    public function slideshow_add($slideshow = '', $slideshow_urll = '')
+    {
+        $url = $this->actionUpload($slideshow);//上传媒体库
+
+        $data['slideshow'] = $url;
+        $data['slideshow_url'] = $slideshow_urll;
+        $data['type'] = 1;
+
+        $into = DB::table($this->_tabName)
+            ->insert($data); 
+
+        return $into;
+    }
+
+/**
+ * 上传图片至素材库
+ * Author Amber
+ * Date 2018-05-08
+ * Params [params]
+ * @param  [type] $img [要处理的图片]
+ */
+    public function actionUpload($img)
+    {
+            $img_name = $img['name'];
+            $url = "./slideshow_img/";//路径
+            move_uploaded_file($img['tmp_name'],  $url . rand() . $file['name']);//原路径，新路径
+            $str =  $url . rand() . $file['name'];
+            return $str;
+     
+    }
+}
---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\HomePageModel;
+
+class HomePageController extends Controller
+{
+    /**
+     * 首页的轮播图 
+     * Author Amber
+     * Date 2018-05-08
+     */
+    public function slideshow(Request $request)
+    {
+        $slideshow_type = $request->input("slideshow_type");
+
+        $HomePageModel = new HomePageModel();
+
+        $ret = $HomePageModel->slideshow($slideshow_type);
+
+        if($ret == FALSE){
+            $res = array(
+                "errNo" => "0003",
+                "errMsg" => "轮播图类型不符"
+            );
+            $this->_response($res);
+        }
+
+        $res = array(
+            "errNo" => 0,
+            'errMsg' => 'success',
+            "data" => $ret
+        );
+
+        $this->_response($res);
+
+    }
+    public function slideshow_add(Request $request){
+
+        $slideshow = $request->input("slideshow");
+        $slideshow_url = $request->input("slideshow_urll");
+
+        $HomePageModel = new HomePageModel();
+
+        $ret = $HomePageModel->slideshow_add( $slideshow, $slideshow_url);
+        if($ret == FALSE){
+            $res = array(
+                "errNo" => "0003",
+                "errMsg" => "系统错误"
+            );
+            $this->_response($res);
+        }
+
+         $res = array(
+                "errNo" => "0",
+                "errMsg" => "添加成功"
+            );
+
+        $this->_response($res);
+    }
+}
